e5331291ada597341cbb05bf0a260ef3f20ec9da
unknown <venkat@codinggeek.com>
2020-12-08 11:40:23 +0530

    dependency injection

--- lib/application/auth/sign_in_form/sign_in_form_bloc.dart
+++ lib/application/auth/sign_in_form/sign_in_form_bloc.dart
@@ -3,14 +3,17 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:sbm/domain/auth/auth_failures.dart';
-import 'package:sbm/domain/auth/i_auth_facade.dart';
-import 'package:sbm/domain/auth/value_objects.dart';
+import 'package:injectable/injectable.dart';
 
+import '../../../domain/auth/auth_failures.dart';
+import '../../../domain/auth/i_auth_facade.dart';
+import '../../../domain/auth/value_objects.dart';
+
+part 'sign_in_form_bloc.freezed.dart';
 part 'sign_in_form_event.dart';
 part 'sign_in_form_state.dart';
-part 'sign_in_form_bloc.freezed.dart';
 
+@injectable
 class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
   final IAuthFacade _authFacade;
   SignInFormBloc(this._authFacade) : super(SignInFormState.initial());
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
+import 'package:sbm/injection.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  configureInjection(Environment.prod);
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
